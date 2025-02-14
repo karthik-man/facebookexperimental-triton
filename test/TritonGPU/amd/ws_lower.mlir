26cc55c35ada3560fc135637fc3e0b77d92fa5e8
Karthik Manivannan <kmanivannan@meta.com>
2025-02-13 19:05:07 -0800

    Init barrier lowering 1

--- test/TritonGPU/amd/ws_lower.mlir
+++ test/TritonGPU/amd/ws_lower.mlir
@@ -1,6 +1,7 @@
 // RUN: triton-opt %s -split-input-file --tritonamdgpu-ws-lowering | FileCheck %s
+
 // CHECK-NOT: triton_nvidia_gpu.get_async_task_id
-// CHECK: foo
+// CHECK-LABEL: foo
 // CHECK: rocdl.workitem.id.x : i32
 // CHECK: arith.divsi %0, %c64_i32 : i32
 
@@ -9,5 +10,18 @@ module attributes {"triton_gpu.num-ctas" = 1 : i32, "triton_gpu.num-warps" = 8 :
   tt.func public @foo() -> i32{
     %14 = triton_nvidia_gpu.get_async_task_id : i32
     tt.return %14 : i32
+  }
+}
+
+// -----
+
+// CHECK-NOT: triton_nvidia_gpu.create_token
+// CHECK-LABEL: foo1
+
+#blocked = #triton_gpu.blocked<{sizePerThread = [4, 4], threadsPerWarp = [8, 8], warpsPerCTA = [2, 4], order = [1, 0]}>
+module attributes {"triton_gpu.num-ctas" = 1 : i32, "triton_gpu.num-warps" = 8 : i32, triton_gpu.target = "hip:gfx942", "triton_gpu.threads-per-warp" = 64 : i32} {
+  tt.func public @foo1() {
+    %2 = triton_nvidia_gpu.create_token {num = 1 : i32} : tensor<1x!triton_nvidia_gpu.token>
+    tt.return
   }
 }
--- third_party/amd/lib/TritonAMDGPUTransforms/WSLowering.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/WSLowering.cpp
@@ -9,6 +9,20 @@
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
 #include "triton/Dialect/TritonGPU/Transforms/Utility.h"
 
+#include "mlir/Transforms/Passes.h"
+#include "triton/Dialect/TritonGPU/Transforms/Passes.h"
+
+#include <set>
+
+#include "mlir/IR/OperationSupport.h"
+#include "triton/Analysis/Utility.h"
+#include "triton/Dialect/Triton/IR/Types.h"
+#include "triton/Dialect/Triton/IR/Utility.h"
+#include "triton/Dialect/TritonGPU/IR/Dialect.h"
+#include "triton/Dialect/TritonGPU/Transforms/Utility.h"
+#include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
+#include "triton/Tools/Sys/GetEnv.hpp"
+
 #define GEN_PASS_CLASSES
 #include "TritonAMDGPUTransforms/Passes.h"
 
@@ -35,6 +49,78 @@ void lowerGetAsyncTaskIdOp(Operation *parentOp) {
     op->erase();
 }
 
+void lowerInit(OpBuilder builder, Location loc, Value barrierCountView, Value barrierPhaseView, int initCount, int phase) {
+  //If thread0, set barrier to initCount
+  auto i32ty = builder.getIntegerType(32);
+  auto zero = builder.create<arith::ConstantIntOp>(loc, 0, 32);
+  auto initCountOp = builder.create<arith::ConstantIntOp>(loc, initCount, 32);
+  auto phaseOp = builder.create<arith::ConstantIntOp>(loc, phase, 32);
+  auto threadId = builder.create<ROCDL::ThreadIdXOp>(loc, i32ty);
+  Value cond = builder.create<arith::CmpIOp>(
+        loc, arith::CmpIPredicate::eq, threadId, zero);
+  auto ifOp = builder.create<scf::IfOp>(loc, cond);
+  auto thenBuilder = ifOp.getThenBodyBuilder();
+  auto countStoreOp =
+      thenBuilder.create<ttg::LocalStoreOp>(loc, initCountOp, barrierCountView);
+  auto phaseStoreOp =
+      thenBuilder.create<ttg::LocalStoreOp>(loc, phaseOp, barrierPhaseView);
+
+}
+
+static const int THREADS_PER_TASK = 64;
+void lowerTokenOperations(Operation *parentOp) {
+  DenseSet<Operation *> eraseOps;
+  parentOp->walk([&](ttng::CreateTokenOp createTokenOp) {
+    llvm::errs() << "post1: " << *parentOp << "\n";
+    MLIRContext *context = createTokenOp.getContext();
+    OpBuilder builder(createTokenOp);
+    Location loc = createTokenOp.getLoc();
+
+    // TBD Check
+    Attribute sharedMemorySpace =
+        triton::gpu::SharedMemorySpaceAttr::get(context);
+    auto barrierCTALayout =
+        ttg::CTALayoutAttr::get(context, /*CTAsPerCGA=*/{1},
+                                /*CTASplitNum=*/{1}, /*CTAOrder=*/{0});
+    auto barrierEncoding =
+        ttg::SharedEncodingAttr::get(context, 1, 1, 1, {0}, barrierCTALayout);
+    
+    Type barrierMemDescType =
+        tt::MemDescType::get({createTokenOp.getNum(), 2}, builder.getI32Type(),
+                             barrierEncoding, sharedMemorySpace,
+                             /*mutableMemory=*/true);
+
+    Type barrierElementMemDescType = tt::MemDescType::get({1}, builder.getI32Type(), barrierEncoding,
+                             sharedMemorySpace, /*mutableMemory=*/true);
+
+    Value bufferFullArray = builder.create<mlir::triton::gpu::LocalAllocOp>(
+        loc, barrierMemDescType, Value());
+    Value bufferEmptyArray = builder.create<mlir::triton::gpu::LocalAllocOp>(
+        loc, barrierMemDescType, Value());
+    auto zero = builder.create<arith::ConstantIntOp>(loc, 0, 32);
+    auto one = builder.create<arith::ConstantIntOp>(loc, 1, 32);
+    for (unsigned i = 0; i < createTokenOp.getNum(); i++) {
+      Value barrierIndex = builder.create<arith::ConstantIntOp>(loc, i, 32);
+      SmallVector<Value> countIdx({barrierIndex, zero});
+      SmallVector<Value> phaseIdx({barrierIndex, one});
+  
+      Value barrierFullCountView = builder.create<ttg::MemDescSubviewOp>(loc, barrierElementMemDescType, bufferFullArray, countIdx);
+      Value barrierFullPhaseView = builder.create<ttg::MemDescSubviewOp>(loc, barrierElementMemDescType, bufferFullArray, phaseIdx);
+      lowerInit(builder, loc, barrierFullCountView, barrierFullPhaseView, THREADS_PER_TASK, 0);
+
+      Value barrierEmptyCountView = builder.create<ttg::MemDescSubviewOp>(loc, barrierElementMemDescType, bufferEmptyArray, countIdx);
+      Value barrierEmptyPhaseView = builder.create<ttg::MemDescSubviewOp>(loc, barrierElementMemDescType, bufferEmptyArray, phaseIdx);
+      lowerInit(builder, loc, barrierEmptyCountView, barrierEmptyPhaseView, THREADS_PER_TASK, 0);
+      eraseOps.insert(createTokenOp);
+    }
+    builder.create<mlir::gpu::BarrierOp>(loc);
+  });
+  for (Operation *op : eraseOps)
+    op->erase();
+}
+
+
+
 class TritonAMDGPUWSLoweringPass
     : public TritonAMDGPUWSLoweringBase<TritonAMDGPUWSLoweringPass> {
 public:
@@ -43,6 +129,7 @@ public:
     ModuleOp mod = getOperation();
     llvm::errs() << "pre: " << mod << "\n";
     lowerGetAsyncTaskIdOp(mod);
+    lowerTokenOperations(mod);
     llvm::errs() << "post: " << mod << "\n";
   }
 };
