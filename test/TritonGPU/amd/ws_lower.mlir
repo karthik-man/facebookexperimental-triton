28cbe0b3f904a03425347b80d1aed521040d1b2e
Karthik Manivannan <kmanivannan@meta.com>
2025-02-21 19:03:59 -0800

    Producer acquire

--- test/TritonGPU/amd/ws_lower.mlir
+++ test/TritonGPU/amd/ws_lower.mlir
@@ -1,27 +1,17 @@
 // RUN: triton-opt %s -split-input-file --tritonamdgpu-ws-lowering | FileCheck %s
 
-// CHECK-NOT: triton_nvidia_gpu.get_async_task_id
-// CHECK-LABEL: foo
-// CHECK: rocdl.workitem.id.x : i32
-// CHECK: arith.divsi %0, %c64_i32 : i32
-
-#blocked = #triton_gpu.blocked<{sizePerThread = [4, 4], threadsPerWarp = [8, 8], warpsPerCTA = [2, 4], order = [1, 0]}>
-module attributes {"triton_gpu.num-ctas" = 1 : i32, "triton_gpu.num-warps" = 8 : i32, triton_gpu.target = "hip:gfx942", "triton_gpu.threads-per-warp" = 64 : i32} {
-  tt.func public @foo() -> i32{
-    %14 = triton_nvidia_gpu.get_async_task_id : i32
-    tt.return %14 : i32
-  }
-}
-
-// -----
-
 // CHECK-NOT: triton_nvidia_gpu.create_token
 // CHECK-LABEL: foo1
+// CHECK-NOT: create_token
+//
 
 #blocked = #triton_gpu.blocked<{sizePerThread = [4, 4], threadsPerWarp = [8, 8], warpsPerCTA = [2, 4], order = [1, 0]}>
 module attributes {"triton_gpu.num-ctas" = 1 : i32, "triton_gpu.num-warps" = 8 : i32, triton_gpu.target = "hip:gfx942", "triton_gpu.threads-per-warp" = 64 : i32} {
   tt.func public @foo1() {
+    %c0_i32_5 = arith.constant 0 : i32
+    %false = arith.constant false
     %2 = triton_nvidia_gpu.create_token {num = 1 : i32} : tensor<1x!triton_nvidia_gpu.token>
+    triton_nvidia_gpu.producer_acquire %2, %c0_i32_5, %false : tensor<1x!triton_nvidia_gpu.token>, i32, i1
     tt.return
   }
 }
--- third_party/amd/lib/TritonAMDGPUTransforms/WSLowering.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/WSLowering.cpp
@@ -22,6 +22,8 @@
 #include "triton/Dialect/TritonGPU/Transforms/Utility.h"
 #include "triton/Dialect/TritonNvidiaGPU/IR/Dialect.h"
 #include "triton/Tools/Sys/GetEnv.hpp"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "triton/Conversion/TritonGPUToLLVM/Utility.h"
 
 #define GEN_PASS_CLASSES
 #include "TritonAMDGPUTransforms/Passes.h"
@@ -49,25 +51,82 @@ void lowerGetAsyncTaskIdOp(Operation *parentOp) {
     op->erase();
 }
 
-void lowerInit(OpBuilder builder, Location loc, Value barrierCountView, Value barrierPhaseView, int initCount, int phase) {
-  //If thread0, set barrier to initCount
-  auto i32ty = builder.getIntegerType(32);
-  auto zero = builder.create<arith::ConstantIntOp>(loc, 0, 32);
-  auto initCountOp = builder.create<arith::ConstantIntOp>(loc, initCount, 32);
-  auto phaseOp = builder.create<arith::ConstantIntOp>(loc, phase, 32);
-  auto threadId = builder.create<ROCDL::ThreadIdXOp>(loc, i32ty);
-  Value cond = builder.create<arith::CmpIOp>(
-        loc, arith::CmpIPredicate::eq, threadId, zero);
-  auto ifOp = builder.create<scf::IfOp>(loc, cond);
-  auto thenBuilder = ifOp.getThenBodyBuilder();
-  auto countStoreOp =
-      thenBuilder.create<ttg::LocalStoreOp>(loc, initCountOp, barrierCountView);
-  auto phaseStoreOp =
-      thenBuilder.create<ttg::LocalStoreOp>(loc, phaseOp, barrierPhaseView);
+void lowerInit(OpBuilder thenBuilder, Location loc, Value barrierCountView,
+               Value barrierPhaseView, int initCount, Value phaseOp,
+               Value threadId) {
+  auto initCountOp =
+      thenBuilder.create<arith::ConstantIntOp>(loc, initCount, 32);
 
+  auto countTensorType = RankedTensorType::get({1}, initCountOp.getType());
+  auto countTensorVal =
+      thenBuilder.create<triton::SplatOp>(loc, countTensorType, initCountOp);
+
+  auto phaseTensorType = RankedTensorType::get({1}, phaseOp.getType());
+  auto phaseTensorVal =
+      thenBuilder.create<triton::SplatOp>(loc, phaseTensorType, phaseOp);
+
+  auto countStoreOp = thenBuilder.create<ttg::LocalStoreOp>(loc, countTensorVal,
+                                                            barrierCountView);
+  auto phaseStoreOp = thenBuilder.create<ttg::LocalStoreOp>(loc, phaseTensorVal,
+                                                            barrierPhaseView);
+}
+Value getMBarrierPhaseBit(OpBuilder &builder, Operation *op,
+                          bool emptyBarrier) {
+  auto loc = op->getLoc();
+  assert(isa<ttng::ProducerAcquireOp>(op) || isa<ttng::ConsumerWaitOp>(op));
+  Value curPhase;
+  if (auto acq = dyn_cast<ttng::ProducerAcquireOp>(op))
+    curPhase = acq.getPhase();
+  else if (auto wait = dyn_cast<ttng::ConsumerWaitOp>(op))
+    curPhase = wait.getPhase();
+  if (emptyBarrier) {
+    // curPhase = curPhase xor True for emptyBarrier.
+    Value _1_1b = builder.create<arith::ConstantIntOp>(loc, 1, 1);
+    curPhase = builder.create<mlir::arith::XOrIOp>(loc, curPhase, _1_1b);
+  }
+  // LLVM_DEBUG(curPhase.dump());
+  return curPhase;
+}
+
+void processProducerAcquireOp(OpBuilder &builder, ttng::ProducerAcquireOp op,
+                              Value bufferEmptyPhaseView) {
+  auto loc = op.getLoc();
+  Value localPhase = getMBarrierPhaseBit(builder, op, true);
+  auto i32Ty = builder.getIntegerType(32);
+  localPhase = builder.create<arith::ExtUIOp>(loc, i32Ty, localPhase);
+  auto phaseTensorType = RankedTensorType::get({1}, builder.getI32Type());
+  auto initialCondition = localPhase;
+  // auto whileOp = builder.create<scf::WhileOp>(loc, initialCondition.getType(),
+  //                                             initialCondition);
+  auto whileOp = builder.create<scf::WhileOp>(loc, TypeRange{}, ValueRange{});
+  // Before block
+  // Before block
+  Block *beforeBlock = builder.createBlock(&whileOp.getBefore());
+  // auto conditionArg = beforeBlock->addArgument(i32Ty, loc);
+  builder.setInsertionPointToEnd(beforeBlock);
+
+  Value barrierPhaseTensor = builder.create<ttg::LocalLoadOp>(
+      loc, phaseTensorType, bufferEmptyPhaseView);
+  auto index_0 = builder.create<arith::ConstantIndexOp>(loc, 0);
+  // SmallVector<Value> index;
+  // index.push_back(index_0);
+  Value barrierPhase =
+      builder.create<mlir::tensor::ExtractOp>(loc, barrierPhaseTensor, ValueRange{index_0});
+  Value phaseCond = builder.create<arith::CmpIOp>(loc, arith::CmpIPredicate::eq,
+                                                  barrierPhase, localPhase);
+  // builder.create<scf::ConditionOp>(loc, phaseCond, beforeBlock->getArguments());
+  builder.create<scf::ConditionOp>(loc, phaseCond, ValueRange{});
+
+  // after block
+  Block *afterBlock = builder.createBlock(&whileOp.getAfter());
+  builder.setInsertionPointToEnd(afterBlock);
+  auto instrinsic = "llvm.amdgcn.s_sleep 10";
+  auto sleepOp = LLVM::createLLVMIntrinsicCallOp(builder, loc, instrinsic, TypeRange{}, ValueRange{});
+  builder.create<scf::YieldOp>(loc,  ValueRange{});
 }
 
 static const int THREADS_PER_TASK = 64;
+static const int WAVES_PER_TASK = 4;
 void lowerTokenOperations(Operation *parentOp) {
   DenseSet<Operation *> eraseOps;
   parentOp->walk([&](ttng::CreateTokenOp createTokenOp) {
@@ -82,13 +141,14 @@ void lowerTokenOperations(Operation *parentOp) {
                                 /*CTASplitNum=*/{1}, /*CTAOrder=*/{0});
     auto barrierEncoding =
         ttg::SharedEncodingAttr::get(context, 1, 1, 1, {0}, barrierCTALayout);
-    
+
     Type barrierMemDescType =
         tt::MemDescType::get({createTokenOp.getNum(), 2}, builder.getI32Type(),
                              barrierEncoding, sharedMemorySpace,
                              /*mutableMemory=*/true);
 
-    Type barrierElementMemDescType = tt::MemDescType::get({1}, builder.getI32Type(), barrierEncoding,
+    Type barrierElementMemDescType =
+        tt::MemDescType::get({1}, builder.getI32Type(), barrierEncoding,
                              sharedMemorySpace, /*mutableMemory=*/true);
 
     Value bufferFullArray = builder.create<mlir::triton::gpu::LocalAllocOp>(
@@ -98,32 +158,64 @@ void lowerTokenOperations(Operation *parentOp) {
 
     auto zero = builder.create<arith::ConstantIntOp>(loc, 0, 32);
     auto one = builder.create<arith::ConstantIntOp>(loc, 1, 32);
-    //Helper function for extracting one element
-    auto createFieldView = [&](Location loc, Value array, int barrierIndex, Value fieldIndex) -> Value {
-      Value barrierIndexOp = builder.create<arith::ConstantIntOp>(loc, barrierIndex, 32);
+    // Helper function for extracting one element
+    auto createFieldView = [&](OpBuilder builder, Location loc, Value array,
+                               Value barrierIndexOp,
+                               Value fieldIndex) -> Value {
       SmallVector<Value> elementIdx({barrierIndexOp, fieldIndex});
-      return builder.create<ttg::MemDescSubviewOp>(loc, barrierElementMemDescType, array, elementIdx);
+      return builder.create<ttg::MemDescSubviewOp>(
+          loc, barrierElementMemDescType, array, elementIdx);
     };
-    //Initialize the barriers
-    for (unsigned barrierIndex = 0; barrierIndex < createTokenOp.getNum(); barrierIndex++) {
-      Value barrierFullCountView = createFieldView(loc, bufferFullArray, barrierIndex, zero);
-      Value barrierFullPhaseView = createFieldView(loc, bufferFullArray, barrierIndex, one);
-      lowerInit(builder, loc, barrierFullCountView, barrierFullPhaseView, THREADS_PER_TASK, 0);
 
-      Value barrierEmptyCountView = createFieldView(loc, bufferEmptyArray, barrierIndex, zero);
-      Value barrierEmptyPhaseView = createFieldView(loc, bufferEmptyArray, barrierIndex, one);
-      lowerInit(builder, loc, barrierEmptyCountView, barrierEmptyPhaseView, THREADS_PER_TASK, 0);
+    // Initialize the barriers
+    //  TBD: Check init counts
+    // If thread0, set barrier to initCount
+    auto i32ty = builder.getIntegerType(32);
+    auto threadId = builder.create<ROCDL::ThreadIdXOp>(loc, i32ty);
+    Value cond = builder.create<arith::CmpIOp>(loc, arith::CmpIPredicate::eq,
+                                               threadId, zero);
+    auto ifOp = builder.create<scf::IfOp>(loc, cond);
+    auto thenBuilder = ifOp.getThenBodyBuilder();
+    Value phaseInitValue = zero;
+    Value phaseOffset = one;
+    Value countOffset = zero;
+
+    for (unsigned barrierIndex = 0; barrierIndex < createTokenOp.getNum();
+         barrierIndex++) {
+      Value barrierIndexOp =
+          thenBuilder.create<arith::ConstantIntOp>(loc, barrierIndex, 32);
+      Value barrierFullCountView = createFieldView(
+          thenBuilder, loc, bufferFullArray, barrierIndexOp, countOffset);
+      Value barrierFullPhaseView = createFieldView(
+          thenBuilder, loc, bufferFullArray, barrierIndexOp, phaseOffset);
+      lowerInit(thenBuilder, loc, barrierFullCountView, barrierFullPhaseView,
+                WAVES_PER_TASK, phaseInitValue, threadId);
+
+      Value barrierEmptyCountView = createFieldView(
+          thenBuilder, loc, bufferEmptyArray, barrierIndexOp, countOffset);
+      Value barrierEmptyPhaseView = createFieldView(
+          thenBuilder, loc, bufferEmptyArray, barrierIndexOp, phaseOffset);
+      lowerInit(thenBuilder, loc, barrierEmptyCountView, barrierEmptyPhaseView,
+                WAVES_PER_TASK, phaseInitValue, threadId);
       eraseOps.insert(createTokenOp);
     }
     builder.create<mlir::gpu::BarrierOp>(loc);
 
+    for (Operation *user : createTokenOp.getResult().getUsers()) {
+      auto loc = user->getLoc();
+      builder.setInsertionPoint(user);
+      if (auto op = dyn_cast<ttng::ProducerAcquireOp>(user)) {
+        Value bufferEmptyPhase = createFieldView(builder, loc, bufferEmptyArray,
+                                                 op.getIdx(), phaseOffset);
+        processProducerAcquireOp(builder, op, bufferEmptyPhase);
+        eraseOps.insert(op);
+      }
+    }
   });
-  
+
   for (Operation *op : eraseOps)
     op->erase();
 }
-
-
 
 class TritonAMDGPUWSLoweringPass
     : public TritonAMDGPUWSLoweringBase<TritonAMDGPUWSLoweringPass> {
@@ -131,7 +223,7 @@ public:
   TritonAMDGPUWSLoweringPass() = default;
   void runOnOperation() override {
     ModuleOp mod = getOperation();
-    llvm::errs() << "pre: " << mod << "\n";
+    // llvm::errs() << "pre: " << mod << "\n";
     lowerGetAsyncTaskIdOp(mod);
     lowerTokenOperations(mod);
     llvm::errs() << "post: " << mod << "\n";
